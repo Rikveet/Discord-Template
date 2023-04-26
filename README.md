02857800b89c513899af2756cbd1add5fa5f3b28
CaptKillSwitch <rh18vo@gmail.com>
2023-04-26 15:13:27 -0400

    Added category command handling, refresing commands on load.

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # Discord V14+ bot Template
 
-# HOW TO RUN
-- Run the following commmand in terminal ``` npm install ```
+### HOW TO RUN
+
+- Run the following commmand in terminal `npm install`
 - Create a .env file in root directory.
-- Add a ``` TOKEN="your discord app token" ``` to the env.
+- Add a `TOKEN="your discord app token"` to the env.
 - If you are using vscode, try the f5 key to run the bot in debug mode.
+
+### HOW TO ADD NEW COMMANDS
+
+- All the commands go inside ./commands/[your category name]. Make sure that your commands follow this pattern as command files in the root folder will not be loaded.
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const { REST, Routes } = require("discord.js");
 const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
 const { Guilds } = GatewayIntentBits;
 const path = require("path");
@@ -9,6 +10,7 @@ const fs = require("fs");
 require("dotenv").config();
 // config writen in .env file of the project
 const TOKEN = process.env.TOKEN;
+const CLIENT_ID = process.env.CLIENT_ID;
 // missing credentials
 if (!TOKEN) {
   throw new Error("Missing token, it is required to initialize your bot");
@@ -19,27 +21,29 @@ if (!TOKEN) {
 // Create a new client instance
 const client = new Client({ intents: [Guilds] });
 
-// Load commands written in ./commands each file is a command
 client.commands = new Collection();
-
-// set path to commands
-const commandsPath = path.join(__dirname, "commands");
-// get all files with .js
-const commandFiles = fs
-  .readdirSync(commandsPath)
-  .filter((file) => file.endsWith(".js"));
-
-// load file and set the command
-for (const file of commandFiles) {
-  const filePath = path.join(commandsPath, file);
-  const command = require(filePath);
-  // Set a new item in the Collection with the key as the command name and the value as the exported module
-  if ("data" in command && "execute" in command) {
-    client.commands.set(command.data.name, command);
-  } else {
-    console.log(
-      `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
-    );
+const commands = [];
+// Grab all the command files from the commands directory you created earlier
+const foldersPath = path.join(__dirname, "commands");
+const commandFolders = fs.readdirSync(foldersPath);
+for (const folder of commandFolders) {
+  // Grab all the command files from the commands directory you created earlier
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith(".js"));
+  // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    if ("data" in command && "execute" in command) {
+      commands.push(command.data.toJSON());
+      client.commands.set(command.data.name, command);
+    } else {
+      console.log(
+        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+      );
+    }
   }
 }
 
@@ -48,11 +52,61 @@ client.once(Events.ClientReady, (_client) => {
   console.log(`Ready! Logged in as ${_client.user.tag}`);
 });
 
+// Deploying the command
+const rest = new REST().setToken(TOKEN);
+(async () => {
+  try {
+    console.log(
+      `Started refreshing ${commands.length} application (/) commands.`
+    );
+
+    // The put method is used to fully refresh all commands in the guild with the current set
+    const data = await rest.put(Routes.applicationCommands(CLIENT_ID), {
+      body: commands,
+    });
+
+    console.log(
+      `Successfully reloaded ${data.length} application (/) commands.`
+    );
+  } catch (error) {
+    // And of course, make sure you catch and log any errors!
+    console.error(error);
+  }
+})();
+
+// Handle interactions
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = client.commands.get(interaction.commandName);
+
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    } else {
+      await interaction.reply({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    }
+  }
+});
+
 // Log in to Discord with your client's token
 client.login(TOKEN);
 
 // avoid crashing the bot
 process.on("uncaughtException", (error, origin) => {
-  console.error(error.message, error.stack);
+  console.error(error.message, error.stack, origin);
 });
-
